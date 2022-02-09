f2237f83d3bc605f36062c2077268f5ab6c7855e
William Bergekrans <williambergekrans@gmail.com>
2022-02-09 15:08:54 +0100

    tested and working  with RPI 2 camera :)

--- live-video-object-detection/live-obj-detection.py
+++ live-video-object-detection/live-obj-detection.py
@@ -8,19 +8,18 @@ net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 
 # Connect to the camera for streaming. 
 # Creates and instance of the videoSource object. 
-camera = jetson.utils.videoSource("csi://0")
+input = jetson.utils.videoSource("csi://0")
 
 # Create video output interface 
-display = jetson.utils.videoOutput("display://0")
+output = jetson.utils.videoOutput("display://0")
 
 # Output to file
-while display.IsStreaming():
-    img = camera.Capture()
-    # Detect automatically overlays the detections on the image. 
+while output.IsStreaming():
+    img = input.Capture()
+    # Detect automatically overlays the detections on the input. 
     detections = net.Detect(img)
-
     # Render the results 
-    display.Render(img)
+    output.Render(img)
     # Display FPS information
-    display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
+    output.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
 
